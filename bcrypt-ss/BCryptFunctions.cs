6d9d14c2aa66c2ba1e1113379dafa4c4c81c61c6
Doug Waldron <douglas.waldron@dnr.ga.gov>
2016-04-28 15:46:35 -0400

    Add wrapper functions for use in SQL Server
    My application does not need all the functionality in BCrypt.Net. Two function's added for HashPassword and CheckPassword

---
+++ bcrypt-ss/BCryptFunctions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.SqlServer.Server;
+using System.Security;
+
+[assembly: AllowPartiallyTrustedCallers]
+
+public class BCryptFunctions
+{
+    /// <summary>
+    /// Verifies that the BCrypt hash of the given text (password) matches the provided hash
+    /// </summary>
+    /// <param name="password">The password to verify</param>
+    /// <param name="hash">The previously-generated hash</param>
+    /// <returns>True if </returns>
+    [SqlFunction()]
+    public static bool CheckPassword(string password, string hash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password.ToString(), hash.ToString());
+    }
+
+    /// <summary>
+    /// Generates a BCrypt hash from a given password
+    /// </summary>
+    /// <param name="password">The password to hash</param>
+    [SqlFunction()]
+    public static string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password.ToString(), BCrypt.Net.BCrypt.GenerateSalt());
+    }
+}
---
+++ bcrypt-ssTests/BCryptFunctionsTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass()]
+    public class BCryptFunctionsUnitTests
+    {
+        [TestMethod()]
+        public void HashPasswordTest()
+        {
+            string h = BCryptFunctions.HashPassword("pass");
+
+            // (Hash should be different every time)
+            Assert.AreNotEqual(BCryptFunctions.HashPassword("pass"), h);
+
+            Assert.AreNotEqual(BCryptFunctions.HashPassword("abc"), h);
+
+            Assert.IsTrue(BCryptFunctions.CheckPassword("pass", h));
+
+            Assert.IsFalse(BCryptFunctions.CheckPassword("abc", h));
+        }
+
+        [TestMethod()]
+        public void CheckPasswordTest()
+        {
+            Assert.IsTrue(BCryptFunctions.CheckPassword("pass", "$2a$10$5V88J4tFtfeouzkjwRiFL.kx.eerDR2T.78pt1dQ7WtBncNnUn5Tm"));
+
+            Assert.IsFalse(BCryptFunctions.CheckPassword("abc", "$2a$10$5V88J4tFtfeouzkjwRiFL.kx.eerDR2T.78pt1dQ7WtBncNnUn5Tm"));
+        }
+    }
+}
+
