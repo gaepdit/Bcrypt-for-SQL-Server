be4d2bca904eb4fb4ecc8d51f9d2a8e61a76df83
Doug Waldron <douglas.waldron@dnr.ga.gov>
2021-08-24 12:04:26 -0400

    Convert to .NET 5 and add SQL Server wrapper functions

---
+++ BCryptSS/Database/CreateDbFunctions.sql
@@ -0,0 +1,58 @@
+﻿SET ANSI_NULLS ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+
+CREATE OR ALTER FUNCTION bcrypt.HashPassword(
+    @password nvarchar(max)
+)
+    RETURNS nvarchar(max)
+
+/*******************************************************************************
+
+Overview:
+Calls the BCrypt HashPassword function in the BCryptSS assembly.
+
+Example usage:
+select bcrypt.HashPassword('abcd');
+
+Input Parameters:
+  @password - user password to hash
+
+Output:
+  hashed password
+
+*******************************************************************************/
+
+    WITH EXECUTE AS CALLER
+AS EXTERNAL NAME
+    BCryptSS.BCryptFunctions.HashPassword;
+GO
+
+CREATE OR ALTER FUNCTION bcrypt.CheckPassword(
+    @password nvarchar(max),
+    @hash     nvarchar(max)
+)
+    RETURNS bit
+
+/*******************************************************************************
+Overview:
+Calls the BCrypt CheckPassword function in the BCryptSS assembly.
+
+Example usage:
+select bcrypt.CheckPassword('abcd','$2a$10$l37yN6i1SVL.4kGAz1v7MOFkZAetcKNJkk2tDAoMFH05KpbsbHZwi');
+
+Input Parameters:
+  @password - user password to compare
+  @hash - password hash to compare
+
+Output:
+  1 if matched
+  0 if not matched
+
+*******************************************************************************/
+
+    WITH EXECUTE AS CALLER
+AS EXTERNAL NAME
+    BCryptSS.BCryptFunctions.CheckPassword;
+GO
--- README.md
+++ README.md
@@ -1,5 +1,22 @@
-# README #
+# BCrypt for SQL Server
 
-A .Net port of jBCrypt implemented in C#. Project originally downloaded from [BCrypt.Net project on CodePlex](https://bcrypt.codeplex.com/) (Last updated Nov 21, 2013).
+This project creates two SQL Server functions that are wrappers around the [BCrypt.Net](https://github.com/BcryptNet/bcrypt.net) functions used to hash and verify passwords.
 
-Created this fork in order to add some changes needed to get BCrypt running on SQL Server.
+* `CheckPassword` calls `BCrypt.Net.BCrypt.Verify`
+* `HashPassword` calls `BCrypt.Net.BCrypt.HashPassword` 
+
+## Usage examples
+
+```sql
+declare
+    @password varchar(57) = 'abc',
+    @hash varchar(max)= '$2a$10$qvcDFHfBnxfX1.1KMIcX4O1UC9yLTkFzq/pQNPT24pm8VudXTnDC2';
+
+select bcrypt.HashPassword(@password);
+select bcrypt.CheckPassword(@password, @hash);
+```
+
+## Installation
+
+1. Add "BCryptSS.dll" as a new assembly in SQL Server.
+2. Run the "Database/CreateDbFunctions.sql" script to add the user defined functions.
